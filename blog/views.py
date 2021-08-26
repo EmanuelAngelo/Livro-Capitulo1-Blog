499e51a3631f9c245156b0d2a27979767ca28fd0
Emanuel Angelo da Silva Almeida <emanuel.almeida@DBOSCO.COM.BR>
2021-08-26 13:54:41 -0300

    att

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,29 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+    # obtem a postagem com base do id
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.method == 'POST':
+        # formulario foi submetido
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # campos de formulario passaram pela validacao
+            cd = form.cleaned_data
+            #envia o email
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_engine = 'blog/post/list.html'
 
 def post_list(request):
     object_list = Post.published.all()
@@ -23,11 +44,3 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     
     return render(request, 'blog/post/detail.html', {'post': post})
-
-
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_engine = 'blog/post/list.html'
