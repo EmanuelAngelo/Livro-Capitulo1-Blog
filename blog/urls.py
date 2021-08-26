a8f4383016cf24ef4601b38824a758178420f0bf
Emanuel Angelo <emanuelangelo@outlook.com.br>
2021-08-25 23:16:43 -0300

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'blog'
+
 urlpatterns = [
 # post views
     path('', views.post_list, name='post_list'),
